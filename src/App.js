47eae448f706fcb2b4a44917b29ea1c243ab80c2
Preethi <preethikotaru5@gmail.com>
2024-07-26 21:49:22 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <BrowserRouter>
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Shop/>}/>
+        <Route path='/FashionFinds' element={<Shop/>}/>
         <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
         <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
         <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid"/>}/>
