f95d87c67ee511db2f64e682cf858039436ca60d
Preethi <preethikotaru5@gmail.com>
2024-07-26 22:01:26 +0530

    deploying

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { HashRouter,Routes,Route } from 'react-router-dom';
 import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
 import Cart  from './Pages/Cart';
@@ -15,7 +15,7 @@ import kids_banner from './Components/Assets/banner_kids.png';
 function App() {
   return (
     <div >
-      <BrowserRouter>
+      <HashRouter>
       <Navbar/>
       <Routes>
         <Route path='/FashionFinds' element={<Shop/>}/>
@@ -30,7 +30,7 @@ function App() {
         <Route path='/login' element={<LoginSignup/>}/>
       </Routes>
       <Footer/>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
