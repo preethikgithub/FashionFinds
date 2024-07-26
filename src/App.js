de54cb0f6998866ea3bc8519a2c664a7032a2966
Preethi <preethikotaru5@gmail.com>
2024-07-26 21:51:43 +0530

    deploying

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/FashionFinds' element={<Shop/>}/>
+        <Route path='/' element={<Shop/>}/>
         <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
         <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
         <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid"/>}/>
